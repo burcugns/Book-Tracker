b802053f2fe8cc504b03ec0ab5861ecd82a0ec2e
Burcu Gunes <burcudagli.10@gmail.com>
2025-09-02 21:52:01 +0100

    updated package and App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-
-
-import ReadingCreate from './components/ReadingCreate'
-import ReadingList from './components/ReadingList'
-import "./App.css"
+import Router from "./Router/Routers";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="min-h-screen bg-neutral-50 py-10 px-4">
-      <div className="max-w-2xl mx-auto">
-        <h1 className="text-3xl font-extrabold text-center tracking-tight text-green-900 mb-8">
-          Book Tracker
-        </h1>
-        <ReadingCreate/>
-        <ReadingList/>
-      </div>
-    </div>
-  )
+  return <Router />;
 }
 
-export default App
+export default App;
+
