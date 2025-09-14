2967c6566a4aee27897821c42b0f72fb13e4a7f6
Burcu Gunes <burcudagli.10@gmail.com>
2025-09-14 21:08:23 +0100

    readingpage updated

--- src/pages/ReadingPage.tsx
+++ src/pages/ReadingPage.tsx
@@ -1,8 +1,35 @@
 import ReadingCreate from "../components/ReadingCreate";
 import ReadingList from "../components/ReadingList";
 import Navbar from "../components/Navbar";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+import { useEffect } from "react";
+import { setReadings } from "../redux/ReadingSlice";
+import { useDispatch } from "react-redux";
 
 function ReadingPage() {
+  const { state } = useLocation();
+  const { data } = state || {};
+  const dispatch = useDispatch();
+
+  async function get_user_books(email: string) {
+    try {
+      const response = await axios.get(
+        `http://127.0.0.1:8000/user_books?email=${email}`
+      );
+      return response.data;
+    } catch (error) {
+      return [];
+    }
+  }
+
+  useEffect(() => {
+    if (!data?.email) return;
+    get_user_books(data.email).then((result) => {
+      dispatch(setReadings(result));
+    });
+  }, [data?.email, dispatch]);
+
   return (
     <div className="min-h-screen bg-neutral-50">
       <Navbar />
@@ -11,7 +38,7 @@ function ReadingPage() {
           Personal Reading List 📚
         </h1>
 
-        <ReadingCreate />
+        <ReadingCreate user_email={data?.email} />
         <ReadingList />
       </div>
     </div>
