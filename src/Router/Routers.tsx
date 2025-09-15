260dc97463280c205753634267d319c3774add71
Burcu Gunes <burcudagli.10@gmail.com>
2025-09-15 21:55:16 +0100

    updated routers

--- src/Router/Routers.tsx
+++ src/Router/Routers.tsx
@@ -2,16 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RegisterFormPage from "../pages/RegisterFormPage";
 import ReadingPage from "../pages/ReadingPage";
 import LoginFormPage from "../pages/LoginPage";
-
-
+import Profile from "../pages/Profile";
+import Settings from "../pages/Settings";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<RegisterFormPage />} />
-        < Route path="/" element={< LoginFormPage />} />
-         <Route path="/readings" element={<ReadingPage />} />
+        <Route path="/" element={<LoginFormPage />} />
+        <Route path="/readings" element={<ReadingPage />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </BrowserRouter>
   );
