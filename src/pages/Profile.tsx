b03f709bfcf705bf0923e97eeecf89b9cb436e60
Burcu Gunes <burcudagli.10@gmail.com>
2025-09-21 18:35:58 +0100

    created profile and settings pages

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+
+export default function Profile() {
+  return (
+    <div>
+      <Navbar />
+      {/* Features will be added later */}
+      <div>Upload your picture</div>
+      <label>
+        <button>Submit</button>
+      </label>
+    </div>
+  );
+}
---
+++ src/pages/Settings.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+function Settings() {
+  return (
+    <div>
+      <Navbar />
+      {/* Features will be added later */}
+      <span>Change your password</span>
+    </div>
+  );
+}
+
+export default Settings;
